601cea653a24d778ef3bed2fa62de33dfdc0a87f
gilyazov-ranel <ranel.yes@gmail.com>
2024-11-17 18:43:35 +0300

    fix

--- terreform-houme-1/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-    name_local = "netology-${yandex_vpc_network.develop.name}-"
-}
--- terreform-houme-1/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "virtual_machina" {
-    value= [
-        {develop-a = ["instance_name: ${yandex_compute_instance.platform-a.name}, external_ip: ${yandex_compute_instance.platform-a.network_interface[0].nat_ip_address}, fqdn: ${yandex_compute_instance.platform-a.fqdn}"]},
-        {develop-b = ["instance_name: ${yandex_compute_instance.platform-b.name}, external_ip: ${yandex_compute_instance.platform-b.network_interface[0].nat_ip_address}, fqdn: ${yandex_compute_instance.platform-b.fqdn}"]}
-    ]
-}
